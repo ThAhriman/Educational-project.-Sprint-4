caf6a86badcd2c2f8bf1f83bad38056ea79af63f
Константин Лищенко <lishchenko91@mail.ru>
2024-04-21 15:21:17 +0300

    fifth-iteration

--- ftracker.go
+++ ftracker.go
@@ -102,7 +102,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	// ваш код здесь
-	meanSpeedInMS := meanSpeed(action, duration) * 1000 / 3600
+	meanSpeedInMS := meanSpeed(action, duration) * 1000.0 / 3600.0
 	spentCalForWalk := ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeedInMS, 2)/(height/100))*walkingSpeedHeightMultiplier*weight) * duration * minInH)
 	return spentCalForWalk
 }
